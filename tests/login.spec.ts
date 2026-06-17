8ad861308253606133e2335027af297307a7ea4c
ngrzegorczyn <ngrzegorczyn@trans.eu>
2026-06-17 14:31:52 +0200

    add login

---
+++ tests/login.spec.ts
@@ -0,0 +1,10 @@
+test("successful login with correct credentials", async ({ page }) => {
+  const url = "https://demo-bank.vercel.app/";
+  const userId = "testerLO";
+  const userPassword = "10987654";
+  await page.goto(url);
+  await page.getByTestId("login-input").fill(userId);
+  await page.getByTestId("password-input").fill(userPassword);
+  await page.getByTestId("login-button").click();
+  await expect(page.getByTestId("user-name")).toHaveText("Jan Demobankowy");
+});
