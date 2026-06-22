d53fddcb9a544bd8e1383610b94dd08d5e6dbc2f
ngrzegorczyn <ngrzegorczyn@trans.eu>
2026-06-22 14:15:35 +0200

    add hange url  to config

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -2,13 +2,12 @@ import { test, expect } from '@playwright/test';
 
 test('successful login with correct credentials', async ({ page }) => {
   //Arrange
-  const url = 'https://demo-bank.vercel.app/';
   const userPassword = '10987654';
   const userId = 'testerLO';
   const correctText = 'Jan Demobankowy';
 
   //Act
-  await page.goto(url);
+  await page.goto('/');
   await page.getByTestId('login-input').fill(userId);
   await page.getByTestId('password-input').fill(userPassword);
   await page.getByTestId('login-button').click();
