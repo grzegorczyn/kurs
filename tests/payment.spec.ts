02231c1d90a087fd5fba9daa47ed9e4ea5e45238
ngrzegorczyn <ngrzegorczyn@trans.eu>
2026-07-08 11:54:54 +0200

    payment tests

---
+++ tests/payment.spec.ts
@@ -0,0 +1,34 @@
+import { test, expect } from '@playwright/test';
+import { loginData, userId } from '../test-data/login.data';
+
+test.describe('Payment tests', () => {
+  test.describe.configure({ retries: 3 });
+  test.beforeEach(async ({ page }) => {
+    const url = 'https://demo-bank.vercel.app/';
+    const userId = loginData.userId;
+    const password = loginData.password;
+
+    await page.goto(url);
+
+    await page.getByTestId('login-input').fill(userId);
+    await page.getByTestId('password-input').fill(password);
+    await page.getByTestId('login-button').click();
+  });
+
+  test('simple payment', async ({ page }) => {
+    //Arrange
+    const odbiorca = 'Katarzyna Super';
+    const accountNumber = '12 3456 7890 0000 0000 0000 00000';
+    const amount = '222';
+//Act
+    await page.getByRole('link', { name: 'płatności' }).click();
+    await page.getByTestId('transfer_receiver').fill(odbiorca);
+    await page.getByTestId('form_account_to').fill(accountNumber);
+    await page.getByTestId('form_amount').fill(amount);
+    await page.getByRole('button', { name: 'wykonaj przelew' }).click();
+
+    //Assert
+    await expect(page.getByRole( 'link', {name:`Przelew wykonany! ${amount},00PLN dla ${odbiorca}`}))
+      
+  });
+});
