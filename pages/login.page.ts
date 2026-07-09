217d04c16e5710d0f920e2095fcd9e3b272efb58
ngrzegorczyn <ngrzegorczyn@trans.eu>
2026-07-09 09:21:28 +0200

    changes POM

--- pages/login.page.ts
+++ pages/login.page.ts
@@ -1,8 +1,13 @@
-import { Page } from '@playwright/test';
+import { Locator, Page } from '@playwright/test';
 
 export class LoginPage {
-  constructor(private page: Page) {}
-  loginInput = this.page.getByTestId('login-input');
-  passwordInput = this.page.getByTestId('password-input');
-  loginButton = this.page.getByTestId('login-button');
+  loginInput: Locator;
+  passwordInput: Locator;
+  loginButton: Locator;
+
+  constructor(private page: Page) {
+    this.loginInput = this.page.getByTestId('login-input');
+    this.passwordInput = this.page.getByTestId('password-input');
+    this.loginButton = this.page.getByTestId('login-button');
+  }
 }
--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 import { loginData, userId } from '../test-data/login.data';
 import { LoginPage } from '../pages/login.page';
 
-test.only('successful login with correct credentials', async ({ page }) => {
+test('successful login with correct credentials', async ({ page }) => {
   //Arrange
 
   const userId = loginData.userId;
@@ -20,3 +20,5 @@ test.only('successful login with correct credentials', async ({ page }) => {
   //Assert
   await expect(page.getByTestId('user-name')).toHaveText(correctText);
 });
+
+
